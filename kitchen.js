b88fa5006829f1ec1cdae5ded698e5aa1c96c80e
Aathirai-whitehat <72262011+Aathirai-whitehat@users.noreply.github.com>
2021-02-08 19:34:08 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,60 @@
+img="";
+status="";
+objects=[];
+
+function preload(){
+    img=loadImage("kitchen.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(600,400);
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd",modelloaded);
+}
+
+
+function modelloaded(){
+    console.log("Model has been loaded");
+    status="True";
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+   
+}
+
+function gotresults(error,results) {
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(img,0,0,600,600);
+    objectdetector.detect(img,gotresults);
+
+    if(status != ""){
+
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        for ( i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Object detected";
+            document.getElementById("number").innerHTML = "No of objects detected: " + objects.length;
+            console.log("hi");
+            accuracy= floor(objects[i].confidence * 100) + " %";
+            fill(r,g,b);
+            text(objects[i].label + " " + accuracy , objects[i].x , objects[i].y);
+            stroke(r,g,b);
+            noFill();
+            rect(objects[i].x , objects[i].y ,objects[i].height , objects[i].width);
+        }
+    }
+
+}
+
+function back(){
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+function room(){
+    window.location = "room.html";
+}
+
+function kitchen(){
+    window.location = "kitchen.html";
+}
+
+function hall(){
+    window.location = "hall.html";
+}
